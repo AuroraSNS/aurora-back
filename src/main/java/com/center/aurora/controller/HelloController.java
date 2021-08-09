a63aa0795b02f0efc69a0a6524c59cfebcffcf39
ccc96360 <ccc96360@naver.com>
2021-08-09 17:46:59 +0900

    Add jenkins test

---
+++ src/main/java/com/center/aurora/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.center.aurora.controller;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        log.debug("Hello API 호출됨 !!!");
+        return "hello~!";
+    }
+}
