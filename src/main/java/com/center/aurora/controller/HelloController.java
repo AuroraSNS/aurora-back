3bdafb29bac100b45377ec84fa09f848d98d4269
ha-jinwoo <gkwlsdn95@naver.com>
2021-08-10 15:01:35 +0900

    Fix hellocontroller

--- src/main/java/com/center/aurora/controller/HelloController.java
+++ src/main/java/com/center/aurora/controller/HelloController.java
@@ -6,12 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slf4j
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        log.debug("Hello API 호출됨 !!!");
-        return "Hello Aurora~!!!";
+        return "Hello Aurora2~!!!";
     }
 }
